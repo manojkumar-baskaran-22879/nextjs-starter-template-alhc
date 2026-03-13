198efa8d8ab6d3dbea95f9379c66238fef51fecb
Manoj Kumar <manojkumar.baskaran@zohocorp.com>
2026-03-13 16:15:15 +0530

    Image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center row-start-2 gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+        <img
+          src="/test.png"
+          alt="Test image"
+          width={300}
+          height={200}
+        />
+
+        <img
+          src="/test.png"
+          alt="Test image"
+          width={300}
+          height={200}
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
@@ -39,3 +42,5 @@ export default function Home() {
     </div>
   );
 }
+
+
